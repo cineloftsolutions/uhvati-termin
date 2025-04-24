e387bae0eb0a01708ac55289afd8b2b8b533b974
cineloftsolutions <office@cineloftsolutions.com>
2025-04-24 10:13:03 +0200

    Initial commit

---
+++ src/main/java/com/cineloftsolutions/uhvati_termin/UhvatiTerminApplication.java
@@ -0,0 +1,13 @@
+package com.cineloftsolutions.uhvati_termin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhvatiTerminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UhvatiTerminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineloftsolutions/uhvati_termin/UhvatiTerminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineloftsolutions.uhvati_termin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhvatiTerminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
