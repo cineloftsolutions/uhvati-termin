833f3e4ec2f66544f0ddefe2bc7f8cc3b0fe344b
vladimir.boskovic <office@cineloftsolutions.com>
2025-04-24 11:03:00 +0200

    Security config, permit all

--- src/main/java/com/cineloftsolutions/uhvati_termin/config/SecurityConfig.java
+++ src/main/java/com/cineloftsolutions/uhvati_termin/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.cineloftsolutions.uhvati_termin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,15 +14,8 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/api/users/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
